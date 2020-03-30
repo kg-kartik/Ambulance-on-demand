b0b2e981c0ed89bdae2a362795ea12e666804b9c
Kartik Goel <goel.kartik39@gmail.com>
2020-03-30 21:00:16 +0530

    Added request schema

---
+++ model/request.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema({
+    requestTime :{
+        type : Date,
+        default : Date.now()
+    },
+    location : {
+        coordinates : {
+           type :  [Number]
+        },
+        address : {
+            type : String
+        }
+    },
+    patientId : {
+        type : String
+    },
+    status : {
+        type : String
+    }
+    
+})
+
+const Request = mongoose.model('Request',requestSchema);
+module.exports = Request;
