fd8104b1f4724b4a314aca7495d75d049d9e2682
Kartik Goel <goel.kartik39@gmail.com>
2020-03-31 21:32:42 +0530

    Sent request to nearby ambulance

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const db = require('./config/keys').MongoURI;
-const ambulanceRoute = require("./routes/ambulance");
-const socketevents = require("./socketevents");
+const requestModel = require("././model/request");
+const myMethods = require("./routes/ambulance");
+const method = myMethods.method;
+const otherMethod = myMethods.otherMethod;
+const hello = myMethods.hello;
 
 mongoose.connect(db, {useNewUrlParser : true,
     useUnifiedTopology: true,
@@ -16,21 +19,69 @@ mongoose.connect(db, {useNewUrlParser : true,
     console.log(err);
 })
 app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
 var server = app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
 });
 
+//Initializing the server instance
 var io =require("socket.io")(server);
 
+//Connecting Users
 io.on('connection', (socket) => {
     console.log("A user just connected");
-    socket.on("request-for-help",(data) => {
-        socket.broadcast.emit("request",data);
+
+    //Ambulance users joinning seperate rooms
+    socket.on('join' ,(data) => {  
+    socket.join(data.displayName);
+    console.log(`User joined room ${data.displayName}`)
+})
+    
+
+//Listening for help event from patient
+//@User Component
+socket.on("request-for-help",(data) => {
+    const requestTime = new Date();
+    const requestId = mongoose.Types.ObjectId();
+    const location = {
+        coordinates : [
+            data.location.longitude,
+            data.location.latitude
+        ]
+    }
+    const patientId = data.patientId;
+    const status = "waiting";
+
+    const request = new requestModel({
+        requestId,
+        requestTime,
+        location,
+        patientId,
+        status
+    })
+
+    //Saving request to the database
+    request.save().then((request) => {
+        console.log("ok");
+    }).catch((err) => {
+        console.log(err);
+    })
+
+    //Fetching nearest ambulance
+    const nearestAmbulance =  otherMethod(76,12,206000);
+    nearestAmbulance.then((result) => {
+        for(let i=0;i<result.length;i++)
+        {
+            io.to(result[i].displayName).emit("request",data);
+            
+        }
+    }).catch((err) => {
+        console.log(err);
+    })
     });
 })
-
 
 
 //for url encoding
@@ -38,12 +89,10 @@ app.use(express.urlencoded({
     extended : false
 }));
 
-
-
 app.use(bodyParser.json());
 
 //Initializing Routes
-app.use('/api/ambulance',ambulanceRoute);
+app.use('/api/ambulance',method);
 
-// const server = http.Server(app);
+
 
