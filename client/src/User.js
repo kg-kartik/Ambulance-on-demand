092e467f1abf8574a5c3fdf3b4527dba13e53b2c
Kartik Goel <goel.kartik39@gmail.com>
2020-04-01 00:55:31 +0530

    Added request-accept-event

--- client/src/User.js
+++ client/src/User.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
-import axios from "axios"
 import socketIOClient from 'socket.io-client'
 
+const socket =  socketIOClient("http://localhost:5000")
 class User extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+        displayName : "",
+        address : ""
+    }
   }
 
+componentDidMount() {
+  socket.on("request-sent",(data) => {
+      console.log(data);
+    this.setState ({
+        displayName : data.displayName,
+        address : data.address
+    })
+  })
+}
 requestforHelp = () => {
     var requestDetails = {
         patientId : "1",
@@ -15,15 +28,16 @@ requestforHelp = () => {
             latitude: 12.9718915,
             longitude: 77.64115449999997
         }
-      }
-  const socket =  socketIOClient("http://localhost:5000")
-  socket.emit("request-for-help",requestDetails)
+    }
+    socket.emit("request-for-help",requestDetails);
 }
-
   render() {
+      const {displayName,address} = this.state;
     return (
       <div>
         <button onClick={this.requestforHelp}> Request for help</button>
+        <h1>{displayName} is coming to take you</h1>
+        <h1> He is here {address}</h1> 
       </div>
     );
   }
